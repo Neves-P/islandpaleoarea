532b4388e5349b1a29323e295968ebf6ffb8b4ea
Neves-P <p.m.santos.neves@rug.nl>
2020-07-12 18:42:05 +0200

    Convert to data-raw folder

--- data-raw/archipelagos23.R
+++ data-raw/archipelagos23.R
@@ -1,3 +1,5 @@
+## code to prepare `archipelagos23` dataset goes here
+
 data(archipelagos41, package = "DAISIE")
 archipelagos_paleo_area <- readr::read_csv(
   "inst/extdata/archipelagos_paleo_area_no_duplicates.csv"
@@ -24,3 +26,5 @@ for (i in seq_along(archipelagos23)) {
   archipelagos23[[i]][[1]]$area <-
     archipelagos_paleo_area$ka16_lambeck[i]
 }
+
+usethis::use_data(archipelagos23, overwrite = TRUE)
