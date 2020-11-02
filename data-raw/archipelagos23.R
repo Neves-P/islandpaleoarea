65e8be858d504bc2040005b44c2571b1aef6d24d
Neves-P <10762799+Neves-P@users.noreply.github.com>
2020-11-02 01:20:42 +0000

    EOD

--- data-raw/archipelagos23.R
+++ data-raw/archipelagos23.R
@@ -4,6 +4,8 @@ data(archipelagos41, package = "DAISIE")
 archipelagos_paleo_area <- readr::read_csv(
   "data-raw/archipelagos_paleo_area_no_duplicates.csv"
 )
+colnames(archipelagos_paleo_area) <- c("archipelago", "ka16_lambeck", "ka16_cutler", "k0_PIAC", "k0_nature")
+archipelagos_paleo_area <- with(archipelagos_paleo_area,  archipelagos_paleo_area[order(archipelago), ])
 archipelago_names <- c()
 
 for (i in seq_along(archipelagos41)) {
@@ -11,19 +13,25 @@ for (i in seq_along(archipelagos41)) {
 }
 
 a <- pmatch(
-  archipelagos_paleo_area[, 1]$archipelagos_paleo_area,
+  archipelagos_paleo_area[, 1]$archipelago,
   archipelago_names
 )
 b <- a[!is.na(a)]
-
-archipelagos23 <- list()
+archipelagos23_current <- list()
 for (i in seq_along(b)) {
-  archipelagos23[i] <- archipelagos41[b[i]]
+  archipelagos23_current[i] <- archipelagos41[b[i]]
 }
 
+archipelagos23_paleo <- archipelagos23_current
 for (i in seq_along(archipelagos23)) {
-  archipelagos23[[i]][[1]]$area <-
+  archipelagos23_paleo[[i]][[1]]$area <-
     archipelagos_paleo_area$ka16_lambeck[i]
 }
 
-usethis::use_data(archipelagos23, overwrite = TRUE)
+archipelagos23_names <- c()
+for (i in seq_along(archipelagos23)) {
+  archipelagos23_names[i] <- archipelagos23_paleo[[i]][[1]]$name
+}
+
+usethis::use_data(archipelagos23_paleo, overwrite = TRUE)
+usethis::use_data(archipelagos23_current, overwrite = TRUE)
