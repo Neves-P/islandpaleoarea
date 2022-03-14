4740575cb662b82cd29966ea2208db35e36319db
Neves-P <p.m.santos.neves@rug.nl>
2022-03-14 13:53:00 +0100

    push

--- R/area_time_plots.R
+++ R/area_time_plots.R
@@ -24,11 +24,77 @@ plot(
   y = log(cutler_summed_unique_areas[1, 2:142]),
   x = x_axis,
   type = "l",
-  ylim = c(0, largest_arch),
-  ylab = "Archipelago area km^2",
+  ylim = c(0, log(largest_arch)),
+  ylab = "log(Archipelago area km^2)",
   xlab = "Ky before present",
-  main = "Paleo-area through time"
+  main = "Paleo-area through time",
+  lwd = 2
 )
 for (i in 2:(nrow(cutler_summed_unique_areas))) {
-  lines(y = cutler_summed_unique_areas[i, 2:142], x, type = "l")
+  lines(y = log(cutler_summed_unique_areas[i, 2:142]), x_axis, type = "l", col = i,
+        lwd = 2)
 }
+
+
+
+# legend(
+#   "bottomleft",
+#   legend = c(cutler_summed_unique_areas[, 1])$archipelago,
+#   col = 1:28,
+#   lty = 1,
+#   ncol = 5
+# )
+out_vec <- c()
+for (i in 3:ncol(cutler_summed_unique_areas)) {
+  out_vec[i - 3] <- cor(cutler_summed_unique_areas[, 2], cutler_summed_unique_areas[, i])
+
+}
+plot(
+  out_vec,
+  type = "l",
+  ylab = "Correlation coeficient vs current area",
+  ylim = c(0, 1),
+  xlab = "Ky before present",
+  main = "Pearson correlation between current area and paleo area",
+  lwd = 2
+)
+
+# Minus outliers
+cutler_summed_unique_areas_no_outliers <- cutler_summed_unique_areas[-21, ]
+cutler_summed_unique_areas_no_outliers <- cutler_summed_unique_areas_no_outliers[-14, ]
+
+
+plot(
+  y = log(cutler_summed_unique_areas_no_outliers[1, 2:142]),
+  x = x_axis,
+  type = "l",
+  ylim = c(0, log(largest_arch)),
+  ylab = "log(Archipelago area km^2)",
+  xlab = "Ky before present",
+  main = "Paleo-area through time",
+  lwd = 2
+)
+for (i in 2:(nrow(cutler_summed_unique_areas_no_outliers))) {
+  lines(y = log(cutler_summed_unique_areas_no_outliers[i, 2:142]), x_axis, type = "l", col = i,
+        lwd = 2)
+}
+
+
+out_vec <- c()
+for (i in 3:ncol(cutler_summed_unique_areas_no_outliers)) {
+  out_vec[i - 3] <-
+    cor(
+      cutler_summed_unique_areas_no_outliers[, 2],
+      cutler_summed_unique_areas_no_outliers[, i]
+    )
+
+}
+plot(
+  out_vec,
+  type = "l",
+  ylab = "Pearson coefficient",
+  ylim = c(0, 1),
+  xlab = "Ky before present",
+  main = "Correlation between current area and paleo area sans outliers",
+  lwd = 2
+)
---
+++ R/more_area_plots.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+area_cutler <- read.csv("G:/O meu disco/PhD/Projects/paleoarea/geographic_data/area_cutler.csv")
+with_hawaii <- ggplot(area_cutler, aes(x = year_before_after_present, y = area_km, color = archipelago_name)) +
+  geom_line()
+ggsave(
+  plot = with_hawaii,
+  filename = "arch_area_with_hawaii.pdf",
+  device = "pdf",
+  width = 25,
+  height = 14,
+  units = "cm"
+)
+
+ area_cutler %>%
+  filter(archipelago_name != 'Hawaii') %>%
+ggplot(aes(x = year_before_after_present, y = area_km, color = archipelago_name)) +
+  geom_line()
+area_cutler %>%
+  filter(archipelago_name != 'Hawaii') %>%
+ggplot(aes(x = year_before_after_present, y = area_km, color = archipelago_name)) +
+  geom_line() + scale_y_continuous(trans = "log2", name = "log2(Archipelago area km^2)")
+
+# Cutler Distortion
+
+ggplot(area_cutler_distortion, aes(x = year_before_after_present, y = area_km, color = archipelago_name)) +
+geom_line()
+
+area_cutler_distortion %>%
+  filter(archipelago_name != 'Hawaii') %>%
+  ggplot(aes(x = year_before_after_present, y = area_km, color = archipelago_name)) +
+  geom_line()
+
+area_cutler_distortion %>%
+  filter(archipelago_name != 'Hawaii') %>%
+  ggplot(aes(x = year_before_after_present, y = area_km, color = archipelago_name)) +
+  geom_line() + scale_y_continuous(trans = "log2", name = "log2(Archipelago area km^2)")
+
