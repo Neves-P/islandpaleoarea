a79807c8d3c0112b0fa6575c0c24f63c2debf6d1
Neves-P <p.m.santos.neves@rug.nl>
2022-06-27 19:08:52 +0200

    Add regular partition script

---
+++ bash/submit_paleo_ml_regular.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --partition=regular
+#SBATCH --array=1-420
+#SBATCH --time=48:00:00
+#SBATCH --mem=1000
+#SBATCH --job-name=paleo_ml
+#SBATCH --output=logs/%x-%j-array-%a.log
+
+mkdir -p logs
+ml R
+
+array_index=$SLURM_ARRAY_TASK_ID
+time_slice=$1
+methode=${2-lsodes}
+optimmethod=${3-subplex}
+
+Rscript --vanilla islandpaleoarea/scripts/paleo_ml.R ${array_index} \
+${time_slice} \
+${methode} \
+${optimmethod}
