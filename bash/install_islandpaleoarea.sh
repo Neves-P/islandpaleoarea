0156ab46b3270b28f46fa6dad0b5bd12067829c5
Neves-P <p.m.santos.neves@rug.nl>
2022-06-27 15:36:48 +0200

    Add install_islandpaleoarea.sh

---
+++ bash/install_islandpaleoarea.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --time=01:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=install_islandpaleoarea
+#SBATCH --output=install_islandpaleoarea.log
+#SBATCH --mem=2GB
+
+mkdir -p logs
+ml R
+Rscript -e "remotes::install_github('rsetienne/DAISIE')"
+Rscript -e "remotes::install_github('tece-lab/DAISIEutils')"
+Rscript -e "remotes::install_github('Neves-P/islandpaleoarea')"
