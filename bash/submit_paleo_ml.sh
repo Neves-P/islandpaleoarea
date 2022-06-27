406c5fd7751062c16e0de2e34a9ca84126373d25
Neves-P <p.m.santos.neves@rug.nl>
2022-06-27 18:47:20 +0200

    Improve submit_paleo_ml

--- bash/submit_paleo_ml.sh
+++ bash/submit_paleo_ml.sh
@@ -3,16 +3,18 @@
 #SBATCH --array=1-420
 #SBATCH --time=48:00:00
 #SBATCH --mem=1000
+#SBATCH --job-name=paleo_ml
+#SBATCH --output=logs/%x-%j-array-%a.log
 
-
+mkdir -p logs
 ml R
 
 array_index=$SLURM_ARRAY_TASK_ID
 time_slice=$1
-methode=$2
-optimmethod=$3
+methode=${2-lsodes}
+optimmethod=${3-subplex}
 
-Rscript --vanilla scripts/paleo_ml.R ${array_index} \
-                                     ${time_slice} \
-                                     ${methode} \
-                                     ${optimmethod}
+Rscript --vanilla islandpaleoarea/scripts/paleo_ml.R ${array_index} \
+                                                     ${time_slice} \
+                                                     ${methode} \
+                                                     ${optimmethod}
