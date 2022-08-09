615d44688194c716473079fe0b7cac1a17d92137
Neves-P <p.m.santos.neves@rug.nl>
2022-08-10 01:31:06 +0200

    Add single model submission longer jobs

---
+++ R/paleo_model_ml.R
@@ -0,0 +1,112 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+library(DAISIE)
+library(islandpaleoarea)
+
+array_index <- as.numeric(args[1])
+time_slice <- as.numeric(args[2])
+model <- as.numeric(args[3])
+methode <- args[4]
+optimmethod <- args[5]
+
+parallel <- "local"
+data_name <- data(archipelagos41_paleo)
+
+seed <- as.integer(Sys.time()) %% 10000L * array_index
+set.seed(
+  seed,
+  kind = "Mersenne-Twister",
+  normal.kind = "Inversion",
+  sample.kind = "Rejection"
+)
+
+DAISIEutils::print_metadata(
+  data_name = paste(data_name, time_slice, sep = "_"),
+  array_index = array_index,
+  model = model,
+  seed = seed,
+  methode = methode,
+  optimmethod = optimmethod
+)
+
+output_folder_path <- DAISIEutils::create_output_folder(
+  data_name = data_name,
+  results_dir = NULL
+)
+
+datalist <- archipelagos41_paleo[[time_slice]]
+
+model_args <- setup_mw_model(model)
+initparsopt <- model_args$initparsopt
+idparsopt <- model_args$idparsopt
+parsfix <- model_args$parsfix
+idparsfix <- model_args$idparsfix
+res <- model_args$res
+ddmodel <- model_args$ddmodel
+cpus <- model_args$cpus
+tol <- model_args$tol
+distance_type <- model_args$distance_type
+distance_dep <- model_args$distance_dep
+
+
+lik_res <- DAISIE::DAISIE_MW_ML(
+  datalist = datalist,
+  initparsopt = initparsopt,
+  idparsopt = idparsopt,
+  parsfix = parsfix,
+  idparsfix = idparsfix,
+  res = res,
+  ddmodel = ddmodel,
+  methode = methode,
+  cpus = cpus,
+  parallel = parallel,
+  optimmethod = optimmethod,
+  tol = tol,
+  distance_type = distance_type,
+  distance_dep = distance_dep
+)
+
+output_path <- file.path(
+  output_folder_path,
+  paste0(
+    data_name,
+    "_",
+    model,
+    "_",
+    time_slice,
+    "_",
+    array_index,
+    ".rds"
+  )
+)
+saveRDS(
+  lik_res,
+  file = output_path
+)
+if (!file.exists(output_path)) {
+  message("Error writing RDS file.")
+}
+to_write <- c(
+  array_index,
+  time_slice,
+  seed,
+  model,
+  distance_dep,
+  distance_type,
+  as.matrix(lik_res),
+  initparsopt,
+  "\n"
+)
+
+file_name <- paste0(
+  "results_",
+  data_name,
+  "_",
+  model,
+  "_",
+  time_slice,
+  "_",
+  array_index,
+  ".txt"
+)
+cat(to_write, file = file.path(output_folder_path, file_name), append = TRUE)
---
+++ bash/submit_paleo_model_ml_regular.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH --partition=regular
+#SBATCH --array=1-15
+#SBATCH --time=4-00:00:00
+#SBATCH --mem=1000
+#SBATCH --cpus-per-task=6
+#SBATCH --job-name=paleo_long
+#SBATCH --output=logs/%x-%j-array-%a.log
+# islandpaleoarea: Paleo-Area Influence on Island Evolutionary Models
+# Copyright (C) 2022 Pedro Santos Neves
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Example job using defaults
+# sbatch islandpaleoarea/bash/submit_paleo_ml_regular.sh 1 17
+# Example job using non-default arguments
+# sbatch islandpaleoarea/bas/submit_paleo_ml_regular.sh 1 lsoda simplex
+
+mkdir -p logs
+ml R
+
+array_index=$SLURM_ARRAY_TASK_ID
+time_slice=$1
+model=$2
+methode=${3-lsodes}
+optimmethod=${4-subplex}
+
+Rscript --vanilla islandpaleoarea/scripts/paleo_model_ml.R ${array_index} \
+${time_slice} \
+${model} \
+${methode} \
+${optimmethod}
