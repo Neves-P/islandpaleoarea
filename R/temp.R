ce92a967b8a57d801f26ee11348f3b012bfe9538
Neves-P <p.m.santos.neves@rug.nl>
2022-08-21 16:15:56 +0200

    rework data in progres

--- R/temp.R
+++ R/temp.R
@@ -3,17 +3,23 @@ ordered_results_paleo <- results_paleo |>
   dplyr::group_by(age, model) |>
   dplyr::arrange(dplyr::desc(bic), .by_group = TRUE) |>
   dplyr::slice_min(order_by = bic)
+best_models <- c(17, 19, 4, 18)
+
 
 ordered_results_paleo <- ordered_results_paleo |>
   dplyr::group_by(age) |>
   dplyr::mutate(bic_weights = calc_bic_weights(bic)) |>
   dplyr::mutate(model = as.factor(model))
 
-best_models <- c(17, 19, 4, 18)
 
-df_to_plot <- ordered_results_paleo |> dplyr::mutate(model = function(x) {
-  if (!(x %in% best_models)) 0
-})
+
+# levels(ordered_results_paleo$model) <- c(0, levels(ordered_results_paleo$model))
+# for (i in seq_along(ordered_results_paleo$model)) {
+#   if (!(ordered_results_paleo$model[i] %in% best_models)) {
+#     ordered_results_paleo$model[i] <- factor(0)
+#   }
+# }
+
 
 
 ggplot2::ggplot(ordered_results_paleo,
