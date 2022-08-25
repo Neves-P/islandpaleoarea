bb27463f2d8605043e796e2b9648a2d48c5f10c8
Neves-P <p.m.santos.neves@rug.nl>
2022-08-26 00:35:56 +0200

    Add plots that make sense

--- scripts/line_plots.R
+++ scripts/line_plots.R
@@ -13,11 +13,17 @@ for (time_slice in seq_along(archipelagos41_paleo)) {
   }
   total_area[time_slice] <- sum(area)
 }
-m_19 <- ordered_results_paleo |> dplyr::filter(model == 19)
-m_19 <- cbind(m_19, total_area = total_area[m_19$age])
+m_15 <- ordered_results_paleo |> dplyr::filter(model == 15)
+m_15 <- cbind(m_15, total_area = total_area[m_15$age])
 
-m_19_plot <- plot_res_area(m_19)
-save_paper_plot(plot_to_save = m_19_plot, file_name = "m_19_plot")
+m_15_plot <- plot_res_area(m_15)
+save_paper_plot(plot_to_save = m_15_plot, file_name = "m_15_plot")
+
+m_16 <- ordered_results_paleo |> dplyr::filter(model == 16)
+m_16 <- cbind(m_16, total_area = total_area[m_16$age])
+
+m_16_plot <- plot_res_area(m_16)
+save_paper_plot(plot_to_save = m_16_plot, file_name = "m_16_plot")
 
 m_17 <- ordered_results_paleo |> dplyr::filter(model == 17)
 m_17 <- cbind(m_17, total_area = total_area[m_17$age])
