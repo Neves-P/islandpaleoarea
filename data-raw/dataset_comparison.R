ba0e3e28c41a9bd1b1373c73ae1297ffb31ab52f
Neves-P <p.m.santos.neves@rug.nl>
2022-08-29 03:01:06 +0200

    Rework data comparison

---
+++ data-raw/dataset_comparison.R
@@ -0,0 +1,7 @@
+## code to prepare `dataset_comparison` dataset goes here
+
+
+path <- system.file("extdata", "dataset_comparison.csv", package = "islandpaleoarea")
+dataset_comparison <- readr::read_csv(path)
+
+usethis::use_data(dataset_comparison, overwrite = TRUE)
--- scripts/dataset_comparison.R
+++ scripts/dataset_comparison.R
@@ -1,23 +1,26 @@
-data("results_nature_current")
-
+data("dataset_comparison")
 
 nature_old_vs_new_data <- dataset_comparison[1:28, -1] - dataset_comparison[29:56, -1]
 nature_comparison <- dataset_comparison[1:28, -1] - dataset_comparison[57:84, -1]
 new_vs_nature_new <- dataset_comparison[29:56, -1] - dataset_comparison[57:84, -1]
 
+View(nature_old_vs_new_data)
+View(nature_comparison)
+View(new_vs_nature_new)
 
-nature_old_vs_new_data <- nature_old_vs_new_data |> tibble::rowid_to_column(var="Model") %>%
-  tidyr::gather(key="Parameter", value="Difference", -1)
+nature_old_vs_new_data <- nature_old_vs_new_data |> tibble::rowid_to_column(var="Model") |>
+  tidyr::gather(key = "Parameter", value = "Difference", -1)
 ggplot2::ggplot(nature_old_vs_new_data, ggplot2::aes(x = Model, y = Parameter, fill = Difference)) +
-  ggplot2::geom_tile()
+  ggplot2::geom_tile() +
+  ggplot2::ggtitle("Nature data vs new data")
 
-nature_comparison <- nature_comparison |> tibble::rowid_to_column(var="Model") %>%
+nature_comparison <- nature_comparison |> tibble::rowid_to_column(var="Model") |>
   tidyr::gather(key = "Parameter", value = "Difference", -1)
 ggplot2::ggplot(nature_comparison, ggplot2::aes(x = Model, y = Parameter, fill = Difference)) +
   ggplot2::geom_tile() +
   ggplot2::ggtitle("Nature data with new\nand old DAISIE")
 
-new_vs_nature_new <- new_vs_nature_new |> tibble::rowid_to_column(var="Model") %>%
+new_vs_nature_new <- new_vs_nature_new |> tibble::rowid_to_column(var="Model") |>
   tidyr::gather(key = "Parameter", value = "Difference", -1)
 ggplot2::ggplot(new_vs_nature_new, ggplot2::aes(x = Model, y = Parameter, fill = Difference)) +
   ggplot2::geom_tile() +
