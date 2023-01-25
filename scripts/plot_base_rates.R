a535473df998b9363226a16412adf4dfe70b731d
Pedro Santos Neves <10762799+Neves-P@users.noreply.github.com>
2023-01-25 03:11:07 +0100

    Finish hyperpars plot, add LL plot

--- scripts/plot_base_rates.R
+++ scripts/plot_base_rates.R
@@ -14,7 +14,8 @@ pars_res_df <- data.frame(
   z = model_res$z,
   alpha = model_res$alpha,
   beta = model_res$beta,
-  d0 = model_res$d_0
+  d0 = model_res$d_0,
+  loglik = model_res$loglik
 )
 
 names(archipelagos41_paleo[[1]])
@@ -79,9 +80,12 @@ for(i in 1:41) {
     ggplot2::theme(legend.position = "bottom")
 }
 
+
+
 out <- patchwork::wrap_plots(combined_plots, guides = "collect", ncol = 6, nrow = 7)  &
   ggplot2::theme(legend.position = "bottom")
-ggsa
+
+
 
 # Combine single arch plots with area curve
 # Plot also the hyperparameters per model
@@ -95,12 +99,97 @@ ggsa
 # 41 Plot with archipelago rates and areas as function of age
 # Combine in 6x7
 
+# Overlay area
 
-# Notes 11/01
+rates_plots <- list()
+combined_plots <- list()
+area_plots <- list()
+hyperpars_plots <- list()
+for(i in 1:41) {
+  rates_plots[[i]] <- plot_line_estimates(base_rates[[i]])
+
+
+  rates_plots[[i]] <- rates_plots[[i]] +
+    # ggplot2::ggplot(base_rates[[i]]) +
+    ggplot2::geom_line(ggplot2::aes(age, x, colour = "x")) +
+    ggplot2::geom_line(ggplot2::aes(age, d0, colour = "d0")) +
+    ggplot2::geom_line(ggplot2::aes(age, beta, colour = "beta")) +
+    ggplot2::geom_line(ggplot2::aes(age, alpha, colour = "alpha")) +
+    ggplot2::geom_line(ggplot2::aes(age, area)) +
+    ggplot2::theme_classic() +
+    ggplot2::xlab("Time before present") +
+    ggplot2::ylab("Hyperparameter")
+
+
+
+  # combined_plots[[i]] <- (area_plots[[i]] + rates_plots[[i]] +
+  #                           patchwork::plot_annotation(
+  #                             title = paste0(gsub("_", " ", names(base_rates[i]), "_"), " m ", model_number)
+  #                           )) +
+  #   ggplot2::scale_fill_continuous(guide = ggplot2::guide_legend()) +
+  #   ggplot2::theme(legend.position = "bottom")
+}
+
+
 
 # 1 plot with hyperparameters through time (global) plus area. This plot is global, as
 # set of hyperpars is global by definition
-# Plot of loglikelihood through time. Is peak in LL the better one? In a panel hyperpars plot
 # Set of model estimates per archipelago (4 or 5, 41 times, one per archipelago)
+# Plot of loglikelihood through time. Is peak in LL the better one? In a panel hyperpars plot
 # Make 3 versions of plots above for each of the 3 best models. Some go in supmat, others are panels
 # Plot with overlay of all 41 areas
+
+
+# Hyperpars plot
+
+## Add here global area plot?
+global_area_time <- c()
+data("ordered_results_paleo")
+
+
+total_area <- c()
+total_distance <- c()
+for (time_slice in seq_along(base_rates[[1]]$age)) { # Loop over ages for which there is data in all models
+  area <- c()
+  distance <- c()
+  for (archipelago in seq_along(archipelagos41_paleo[[time_slice]])) {
+    area <- c(area, archipelagos41_paleo[[time_slice]][[archipelago]][[1]]$area)
+    distance <- c(distance, archipelagos41_paleo[[time_slice]][[archipelago]][[1]]$distance_continent)
+
+  }
+  total_area[time_slice] <- sum(area)
+}
+hyperpars_df <- data.frame(base_rates[[1]], total_area)
+max_hyperpars <- max(
+  hyperpars_df$x,
+  hyperpars_df$d0,
+  hyperpars_df$beta,
+  hyperpars_df$alpha
+)
+peak_area <- max(total_area)
+scale_factor <- peak_area / max_hyperpars
+hyperpars_plot <- ggplot2::ggplot(hyperpars_df) +
+  ggplot2::geom_line(ggplot2::aes(age, x, colour = "x")) +
+  ggplot2::geom_line(ggplot2::aes(age, d0, colour = "d0")) +
+  ggplot2::geom_line(ggplot2::aes(age, beta, colour = "beta")) +
+  ggplot2::geom_line(ggplot2::aes(age, alpha, colour = "alpha")) +
+  ggplot2::geom_line(ggplot2::aes(age, total_area / scale_factor, colour = "total area")) +
+  ggplot2::scale_y_continuous(
+    name = "Hyperparameters",
+    sec.axis = ggplot2::sec_axis(~.*scale_factor, name = "Area")
+  ) +
+  ggplot2::theme_classic() +
+  ggplot2::theme(legend.title = ggplot2::element_blank()) +
+  ggplot2::xlab("Time before present") +
+  ggplot2::ylab("Hyperparameter")
+hyperpars_plot
+
+
+# LL plot
+loglik_plot <- ggplot2::ggplot(pars_res_df) +
+  ggplot2::geom_line(ggplot2::aes(age, loglik, colour = "loglik")) +
+  ggplot2::theme_classic() +
+  ggplot2::theme(legend.title = ggplot2::element_blank()) +
+  ggplot2::xlab("Time before present") +
+  ggplot2::ylab("Loglikelihood")
+
