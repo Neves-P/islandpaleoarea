8b58ab358a2e2dcc990549e427d5b5307ab00c8d
Neves-P <p.m.santos.neves@rug.nl>
2023-08-02 17:55:05 +0200

    Add minus_arch WIP

---
+++ scripts/paleo_ml_minus_arch.R
@@ -0,0 +1,114 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+library(DAISIE)
+library(islandpaleoarea)
+
+array_index <- as.numeric(args[1])
+time_slice <- as.numeric(args[2])
+methode <- args[3]
+optimmethod <- args[4]
+model <- array_index
+parallel <- "local"
+
+data_name <- data(archipelagos41_paleo)
+
+seed <- as.integer(Sys.time()) %% 10000L * array_index
+set.seed(
+  seed,
+  kind = "Mersenne-Twister",
+  normal.kind = "Inversion",
+  sample.kind = "Rejection"
+)
+
+DAISIEutils::print_metadata(
+  data_name = paste(data_name, model, time_slice, "minus_arch", sep = "_"),
+  array_index = "minus_arch",
+  model = model,
+  seed = seed,
+  methode = methode,
+  optimmethod = optimmethod
+)
+
+output_folder_path <- DAISIEutils::create_output_folder(
+  data_name = data_name,
+  results_dir = NULL
+)
+
+
+previous_data <- data("ordered_results_paleo", package = "islandpaleoarea")
+
+prev_time_slice <- time_slice - 1
+
+best_previous_time_slice <- dplyr::filter(
+  ordered_results_paleo,
+  age == prev_time_slice,
+  model == model
+)
+
+if (all(is.na(bics))) {
+  stop("Files found, but no valid previous results available.")
+}
+
+message("Using parameters from preceeding time slice.")
+message("Files to read were: ", files_to_read)
+message("The current time slice is: ", time_slice)
+message("The previous time slice is: ", prev_time_slice)
+message("The previous time slice initpars are: ", paste(unlist(best_previous_time_slice[4:16]), collapse = " "))
+
+
+
+message("Using parameters from preceeding time slice.")
+message("Archipelago removed: ", files_to_read)
+message("The current time slice is: ", time_slice)
+message("The previous time slice is: ", time_slice - 1)
+message("The previous time slice initpars are: ", paste(unlist(best_previous_time_slice[4:16]), collapse = " "))
+
+datalist <- archipelagos41_paleo[[time_slice]]
+
+model_args <- setup_mw_model_fixed_pars(model, best_previous_time_slice)
+initparsopt <- model_args$initparsopt
+idparsopt <- model_args$idparsopt
+parsfix <- model_args$parsfix
+idparsfix <- model_args$idparsfix
+res <- model_args$res
+ddmodel <- model_args$ddmodel
+cpus <- model_args$cpus
+tol <- model_args$tol
+distance_type <- model_args$distance_type
+distance_dep <- model_args$distance_dep
+
+
+lik_res <- DAISIE::DAISIE_MW_ML(
+  datalist = datalist,
+  initparsopt = initparsopt,
+  idparsopt = idparsopt,
+  parsfix = parsfix,
+  idparsfix = idparsfix,
+  res = res,
+  ddmodel = ddmodel,
+  methode = methode,
+  cpus = cpus,
+  parallel = parallel,
+  optimmethod = optimmethod,
+  tol = tol,
+  distance_type = distance_type,
+  distance_dep = distance_dep
+)
+
+output_path <- file.path(
+  output_folder_path,
+  paste0(
+    data_name,
+    "_",
+    model,
+    "_",
+    time_slice,
+    "_minus_arch.rds")
+)
+saveRDS(
+  lik_res,
+  file = output_path
+)
+if (!file.exists(output_path)) {
+  message("Error writing RDS file.")
+}
