5337c26117b8e8d6e987f89afac189a6ac1f7200
Neves-P <p.m.santos.neves@rug.nl>
2023-12-03 22:39:34 +0100

    Reworked plot

--- R/plot_model_sel_time.R
+++ R/plot_model_sel_time.R
@@ -11,6 +11,7 @@
 #' model_sel_plot <- plot_model_sel_time(ordered_results_paleo)
 plot_model_sel_time <- function(ordered_results,
                                 best_models = c(17, 19, 18)) {
+
   levels(ordered_results$model) <- c(levels(ordered_results$model), "Other\nmodels")
   for (i in seq_along(ordered_results$model)) {
     if (!(ordered_results$model[i] %in% best_models)) {
@@ -20,11 +21,24 @@ plot_model_sel_time <- function(ordered_results,
   plot_data_frame <- data.frame(
     age = numeric(),
     model = numeric(),
-    bic_weights = numeric()
+    bic_weights = numeric(),
+    total_area = numeric()
   )
+  total_area <- c()
+  for (time_slice in seq_along(archipelagos41_paleo)) {
+    area <- c()
+    distance <- c()
+    for (archipelago in seq_along(archipelagos41_paleo[[time_slice]])) {
+      area <- c(area, archipelagos41_paleo[[time_slice]][[archipelago]][[1]]$area)
+      distance <- c(distance, archipelagos41_paleo[[time_slice]][[archipelago]][[1]]$distance_continent)
+
+    }
+    total_area[time_slice] <- sum(area)
+  }
+  max_total_area <- max(total_area)
   for (age_slice in unique(ordered_results$age)) {
     lines_to_change <- which(ordered_results$age == age_slice)
-    subset_df <- ordered_results[lines_to_change, ]
+    subset_df <- cbind(ordered_results[lines_to_change, ])
     good_model_lines <- which(subset_df$model != "Other\nmodels")
     good_weights <- sum(subset_df$bic_weights[good_model_lines])
     bad_model_lines <- which(subset_df$model == "Other\nmodels")
@@ -32,12 +46,14 @@ plot_model_sel_time <- function(ordered_results,
     testit::assert(bad_model_weights >= 0)
     lines_to_keep <- cbind(
       subset_df[good_model_lines, 1:2],
-      subset_df[good_model_lines, "bic_weights"]
+      subset_df[good_model_lines, "bic_weights"],
+      total_area = total_area[age_slice] / max_total_area
     )
     line_to_insert <- data.frame(
       age = age_slice,
       model = factor("Other\nmodels"),
-      bic_weights = bad_model_weights
+      bic_weights = bad_model_weights,
+      total_area = total_area[age_slice] / max_total_area
     )
     plot_data_frame <- rbind(plot_data_frame, lines_to_keep, line_to_insert)
     testit::assert(identical(sum(
@@ -48,14 +64,22 @@ plot_model_sel_time <- function(ordered_results,
     )
     )
   }
-  out <- ggplot2::ggplot(plot_data_frame,
-                         ggplot2::aes(x = age, y = bic_weights, fill = model)) +
-    ggplot2::geom_area() +
+  # Need to rescale this
+  out <- ggplot2::ggplot() +
+    ggplot2::geom_area(data = plot_data_frame, ggplot2::aes(x = age, y = bic_weights, fill = model)) +
     ggplot2::theme_classic() +
     ggplot2::scale_fill_manual(values = c("#CAB2D6", "#FFFF99", "#A6CEE3", "#FB9A99")) +
     ggplot2::xlab("Age (ka)") +
     ggplot2::ylab("BIC weight") +
-    ggplot2::guides(fill = ggplot2::guide_legend(title = "Model"))
-  out
+    ggplot2::guides(fill = ggplot2::guide_legend(title = "Model")) +
+    ggplot2::scale_y_continuous(
+      name = "BIC weight",
+      sec.axis = ggplot2::sec_axis(~.*max_total_area, name = "Total archipelago area km\U00B2")
+    ) +
+    ggplot2::geom_line(data = plot_data_frame, ggplot2::aes(x = age, y = total_area))
+
+
+
+
 }
 
