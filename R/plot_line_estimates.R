bb324b8a7fd6eb2e47e73f2081fef63b5b3eb76f
Neves-P <10762799+Neves-P@users.noreply.github.com>
2023-12-27 17:02:53 +0000

    Use italics

--- R/plot_line_estimates.R
+++ R/plot_line_estimates.R
@@ -21,12 +21,14 @@ plot_line_estimates <- function(ordered_results, standardisation = FALSE, log_ga
     ordered_results$gamma_0 <- log(ordered_results$gamma_0)
   }
   param_plot <- ggplot2::ggplot(ordered_results) +
-    ggplot2::geom_line(ggplot2::aes(age, lambda_c0, colour = "\U03BB\U1D9C")) +
-    ggplot2::geom_line(ggplot2::aes(age, mu_0, colour = "\U03BC")) +
-    ggplot2::geom_line(ggplot2::aes(age, lambda_a0, colour = "\U03BB\U1D43")) +
-    ggplot2::geom_line(ggplot2::aes(age, gamma_0, colour = "\U03B3")) +
+    ggplot2::geom_line(ggplot2::aes(age, lambda_c0, colour = "*\U03BB\U1D9C*")) +
+    ggplot2::geom_line(ggplot2::aes(age, mu_0, colour = "*\U03BC*")) +
+    ggplot2::geom_line(ggplot2::aes(age, lambda_a0, colour = "*\U03BB\U1D43*")) +
+    ggplot2::geom_line(ggplot2::aes(age, gamma_0, colour = "*\U03B3*")) +
     ggplot2::theme_classic() +
-    ggplot2::theme(legend.title = ggplot2::element_blank()) +
+    ggplot2::theme(legend.title = ggplot2::element_blank(),
+                   legend.text = ggtext::element_markdown(),
+                   axis.title.y = ggtext::element_markdown()) +
     ggplot2::xlab("Time before present") +
     ggplot2::ylab("Estimated rates") +
     ggplot2::scale_color_brewer(palette = "Set2")
@@ -37,7 +39,7 @@ plot_line_estimates <- function(ordered_results, standardisation = FALSE, log_ga
     #   sec.axis = ggplot2::sec_axis(~log(.), name = "Estimated log(\U03B3)", breaks = seq(1, 4, 0.1))
     # )
     param_plot <- param_plot + ggplot2::scale_y_continuous(
-      name = "Estimated \U03BB\U1D9C; \U03BC; \U03BB\U1D43; log(\U03B3)"
+      name = "Estimated *\U03BB\U1D9C*; *\U03BC*; *\U03BB\U1D43*; log(*\U03B3*)"
     )
   }
   param_plot
